c97fd299f90a05302fbeac9ff2caf76eb8719a35
NgoGManh <manhhyo3103@gmail.com>
2022-11-01 22:58:21 +0700

    Create Program.cs

---
+++ NMLT TH/Week 3/Bai14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bai14 {
+	class Program {
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			int n;
+			do {
+				Console.Write("Nhap 1 <= n <= 1000: ");
+				n = int.Parse(Console.ReadLine());
+			}
+			while (n < 1 || n > 1000);
+
+			int qua = 0;
+			int s = n;
+
+			do {
+				qua = (s / 10) * 3 + qua;
+				s = s % 10 + qua;
+			}
+			while (s >= 10);
+			Console.WriteLine($"Tổng số chai uống là : {qua}, {n}");
+		}
+	}
+}
