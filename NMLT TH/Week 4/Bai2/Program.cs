a7a2cbd070f6195693b1e982a0eea0057ed02fae
NgoGManh <manhhyo3103@gmail.com>
2022-11-06 00:38:21 +0700

    Update Program.cs

--- NMLT TH/Week 4/Bai2/Program.cs
+++ NMLT TH/Week 4/Bai2/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace Bai2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace Bai2 {
+	class Program {
+		static void HoanDoi(ref int a, ref int b) {
+			int temp = a;
+			a = b;
+			b = temp;
+			Console.WriteLine($"Trong hàm hoán đổi : a = {a}, b = {b}");
+		}
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Nhập a và b");
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+			Console.WriteLine($"Trước Khi Hoán Đổi : a = {a}, b = {b}");
+			HoanDoi(ref a, ref b);
+			Console.WriteLine($"Sau Khi Hoán Đổi : a = {a}, b = {b}");
+		}
+	}
 }
