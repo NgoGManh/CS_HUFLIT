8e5fee58e5cc109687fa2639694063b313b04b8a
NgoGManh <manhhyo3103@gmail.com>
2022-11-08 23:57:11 +0700

    Create Program.cs

---
+++ NMLT TH/Week 4/Bai5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bai5 {
+	class Program {
+		static int TimMax(int a, int b, int c) {
+			int max = a;
+			if (max < b) {
+				max = b;
+			}
+			if (max < c) {
+				max = c;
+			}
+			return max;
+		}
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Nhập a, b, c");
+			int a, b, c;
+			a = int.Parse(Console.ReadLine());
+			b = int.Parse(Console.ReadLine());
+			c = int.Parse(Console.ReadLine());
+			Console.WriteLine($"Số lớn nhất trong 3 số {a}, {b}, {c} là: " + TimMax(a, b, c));
+			Console.ReadKey();
+		}
+	}
+}
