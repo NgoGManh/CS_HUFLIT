f5fdfdc93ff367e6cbb59aa96f74bbf4733a278c
NgoGManh <manhhyo3103@gmail.com>
2022-11-08 23:57:03 +0700

    Create Program.cs

---
+++ NMLT TH/Week 4/Bai8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bai8 {
+	class Program {
+		static int TinhTong(int n) {
+			int sotachra;
+			int s = 0;
+			for (; n != 0;) {
+				sotachra = n % 10;
+				s += sotachra;
+				n /= 10;
+			}
+			return s;
+		}
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Nhập n");
+			int n = int.Parse(Console.ReadLine());
+			Console.WriteLine("Tổng các chữ số = " + TinhTong(n));
+			Console.ReadKey();
+		}
+	}
+}
