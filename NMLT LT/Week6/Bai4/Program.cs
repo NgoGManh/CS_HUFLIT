eec9c2b4035d6cc50f082ed19d8433046f0af8f6
NgoGManh <manhhyo3103@gmail.com>
2022-11-17 23:39:38 +0700

    Create Program.cs

---
+++ NMLT LT/Week6/Bai4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bai4 {
+	class Program {
+		static void TaoMang() {
+			Console.WriteLine("Nhập số phần tử của mảng.");
+			int n = int.Parse(Console.ReadLine());
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				Console.WriteLine($"Nhập a[{i}] : ");
+				a[i] = int.Parse(Console.ReadLine());
+			}
+		}
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			TaoMang();
+		}
+	}
+}
