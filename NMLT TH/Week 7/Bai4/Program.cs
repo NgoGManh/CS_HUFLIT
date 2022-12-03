89fdf98e034e3d7c9f20df9a0c58cb7a67ac7999
NgoGManh <manhhyo3103@gmail.com>
2022-12-03 17:34:27 +0700

    Create Program.cs

---
+++ NMLT TH/Week 7/Bai4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bai4 {
+	class Program {
+		static void TimViTri(double[] a, double x) {
+			for (int i = 0; i < a.Length; i++) {
+				Console.WriteLine($"Nhập a[{i}] ");
+				a[i] = double.Parse(Console.ReadLine());
+			}
+
+			for (int i = 0; i < a.Length; i++) {
+				if (a[i] == x) {
+					Console.WriteLine($"Vị trí của phần tử có giá trị x là: a[{i}]");
+				}
+			}
+		}
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Nhập số phần tử của mảng.");
+			int n = int.Parse(Console.ReadLine());
+			double[] a = new double[n];
+			Console.WriteLine($"Nhập x");
+			double x = double.Parse(Console.ReadLine());
+			TimViTri(a, x);
+		}
+	}
+}
