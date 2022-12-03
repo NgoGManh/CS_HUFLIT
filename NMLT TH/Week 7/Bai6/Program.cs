935f4d5f2c9cac707710e5f3067d598cc09bd865
NgoGManh <manhhyo3103@gmail.com>
2022-12-03 17:34:35 +0700

    Create Program.cs

---
+++ NMLT TH/Week 7/Bai6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bai6 {
+	class Program {
+		static void TimViTri(double[] a) {
+			for (int i = 0; i < a.Length; i++) {
+				Console.WriteLine($"Nhập a[{i}] ");
+				a[i] = double.Parse(Console.ReadLine());
+			}
+			double min = a[0];
+			for (int i = 0; i < a.Length; i++) {
+				if (a[i] < min) {
+					min = a[i];
+				}
+			}
+			for (int i = 0; i < a.Length; i++) {
+				if (a[i] == min) {
+					Console.WriteLine($"Vị trí của giá trị nhỏ nhất trong mảng là: a[{i}]");
+				}
+			}
+		}
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.WriteLine("Nhập số phần tử của mảng.");
+			int n = int.Parse(Console.ReadLine());
+			double[] a = new double[n];
+			TimViTri(a);
+		}
+	}
+}
