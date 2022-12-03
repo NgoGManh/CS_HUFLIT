e8e5c1fbc730236630bd1ead90aa002d5d8090aa
NgoGManh <manhhyo3103@gmail.com>
2022-12-03 17:34:42 +0700

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,44 +2,35 @@
 
 namespace Test {
 	class Program {
-		static bool[] TimGiaTri(int[] a) {
-			int max = a[0];
-			for (int i = 1; i < a.Length; i++) {
-				if (a[i] > max) {
-					max = a[i];
-					Console.WriteLine("max = " + max);
-				}
-			}
-			bool[] states = new bool[max + 1];
-			for (int i = 0; i < a.Length; i++) {
-				Console.WriteLine($"states[{a[i]}] truoc khi danh dau = " + states[a[i]]);
-				if (states[a[i]] == false) {
-					states[a[i]] = true;
-					Console.WriteLine($"states[{a[i]}] sau khi danh dau = " + states[a[i]]);
-				}
-			}
-			return states;
-		}
 		static void Main(string[] args) {
 			Console.OutputEncoding = System.Text.Encoding.UTF8;
 			Console.WriteLine("Nhập số phần tử của mảng.");
 			int n = int.Parse(Console.ReadLine());
 			int[] a = new int[n];
-			for (int i = 0; i < n; i++) {
-				Console.WriteLine($"Nhập a[{i}] : ");
+			NhapMang(a);
+			XuatMang(a);
+			Console.WriteLine($"Tổng số chữ số của các phần tử trong mảng là: " + TongSoChuSo(a));
+		}
+		static void NhapMang(int[] a) {
+			for (int i = 0; i < a.Length; i++) {
+				Console.WriteLine($"Nhập a[{i}]");
 				a[i] = int.Parse(Console.ReadLine());
 			}
-
-			int sum = 0;
-			bool[] trangthai = TimGiaTri(a);
-			Console.WriteLine("do dai trangthai =" + trangthai.Length);
-			for (int i = 1; i < trangthai.Length; i++) {
-				Console.WriteLine($"trangthai[{i}] = {trangthai[i]}");
-				if (trangthai[i] == true) {
-					sum += 1;
-				}
+		}
+		static void XuatMang(int[] a) {
+			for (int i = 0; i < a.Length; i++) {
+				Console.WriteLine($"a[{i}] = {a[i]}");
 			}
-			Console.WriteLine("Số phần tử có giá trị phân biệt là: " + sum);
+		}
+		static int TongSoChuSo(int[] a) {
+			int reminder, sum = 0, x;
+			for (int i = 0; i < a.Length; i++) {
+				x = a[i];
+				reminder = x % 10;
+				sum += reminder;
+				x = x / 10;
+			}
+			return sum;
 		}
 	}
 }
