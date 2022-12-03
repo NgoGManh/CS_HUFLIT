665912ff6af987c4674acbc30b54e447e8b5c908
NgoGManh <manhhyo3103@gmail.com>
2022-12-03 17:33:44 +0700

    Create Program.cs

---
+++ NMLT LT/Week7/Bai3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bai3 {
+	class Program {
+		static void Main(string[] args) {
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			int n;
+			Console.WriteLine("Nhập n.");
+			n = int.Parse(Console.ReadLine());
+			bool[] sum = new bool[n + 1];
+			for (int i = 1; i <= n; i++) {
+				sum[i] = false;
+			}
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
